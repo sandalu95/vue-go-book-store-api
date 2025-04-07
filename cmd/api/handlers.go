a2fa9bcfd57b2ec1112f4e23a880a33aa3f56ee4
Umesha Sandalu (Sandy) <sandalukalpanee@gmail.com>
2025-04-07 20:59:55 +1000

    improve routes

---
+++ cmd/api/handlers.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type jsonResponse struct {
+	Error  bool   `json:"error"`
+	Message string `json:"message"`
+}
+
+func (app *application) Login(w http.ResponseWriter, r *http.Request) {
+	type credentials struct {
+		Username string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	var creds credentials
+	var payload jsonResponse
+
+	err:= json.NewDecoder(r.Body).Decode(&creds)
+	if err != nil {
+		app.errorLog.Println("Invalid json")
+
+		payload.Error = true
+		payload.Message = "Invalid json"
+
+		out, err := json.MarshalIndent(payload, "", "\t")
+		if err != nil {
+			app.errorLog.Println(err)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(out)
+		return
+	}
+
+	app.infoLog.Println(creds.Username, creds.Password)
+	payload.Error = false
+	payload.Message = "Login successful"
+	out, err := json.MarshalIndent(payload, "", "\t")
+	if err != nil {
+		app.errorLog.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(out)
+
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module vue-go-book-store-api
 
 go 1.24.1
+
+require github.com/go-chi/chi/v5 v5.2.1 // indirect
